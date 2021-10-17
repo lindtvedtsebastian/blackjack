7e15a395db4d703233f6babdaf35c0413725731f
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-10-17 08:43:34 +0200

    Added Kdoc for Deck

--- app/src/main/kotlin/blackjack/Deck.kt
+++ app/src/main/kotlin/blackjack/Deck.kt
@@ -1,5 +1,11 @@
 package blackjack
 
+
+/**
+ * Deck
+ *
+ * @constructor Create empty Deck
+ */
 class Deck {
     var deck: MutableList<Card>;
 
@@ -7,12 +13,21 @@ class Deck {
         deck = Generate()
     }
 
+
+    /**
+     * Prints the deck 
+     */
     fun Print() {
         for (card in deck) {
             print("${card.PrintableFormat()} ")
         }
     }
-    
+
+
+    /**
+     * Generates a new sorted Deck 
+     * @return - The generated deck 
+     */
     fun Generate(): MutableList<Card> {
         var deck = mutableListOf<Card>()
         for (suit in enumValues<Card.Suit>()) {
@@ -23,23 +38,45 @@ class Deck {
         return deck
     }
 
+    /**
+     * Shuffles the currently stored deck 
+     */
     fun Shuffle() {
         deck.shuffle()
     }
 
+    /**
+     * Card
+     *
+     * @constructor A new Card with a suit and value 
+     *
+     * @param suit - The suit of the card
+     * @param value - The value of the card 
+     */
     class Card(suit: Suit, value: Value) {
         val suit: Suit;
         val value: Value;
-        
+
         init {
             this.suit = suit;
             this.value = value;
         }
 
+        /**
+         * Printable format
+         *
+         * @return The card object in a printable format 
+         */
         fun PrintableFormat(): String {
             return "${suit.printableFormat}${value.printableFormat}"
         }
 
+        /**
+         * Suit
+         *
+         * @property printableFormat How the Suit should be formatted when printed 
+         * @constructor Create new Suit
+         */
         enum class Suit(val printableFormat: String) {
             Clubs("C"),
             Diamonds("D"),
@@ -47,6 +84,13 @@ class Deck {
             Spades("S");
         }
 
+        /**
+         * Value
+         *
+         * @property value - The value of the card in Int form 
+         * @property printableFormat - How the value should be formatted when printed 
+         * @constructor Create new Value
+         */
         enum class Value(val value: Int, val printableFormat: String) {
             Two(2,"2"),
             Three(3,"3"),
